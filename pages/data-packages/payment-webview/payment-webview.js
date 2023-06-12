e0e3a2b1f113dcab448e266674a7464ba6cdfb94
daizhihong <1106869711@qq.com>
2023-06-12 18:27:31 +0800

    navigate

--- pages/data-packages/payment-webview/payment-webview.js
+++ pages/data-packages/payment-webview/payment-webview.js
@@ -1,4 +1,11 @@
 Page({
   data: {},
-  onLoad() {},
+  onLoad(query) {
+    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+    const {
+      id
+    } = query;
+    this.data.url = `https://portalpagos.claro.com.co/index.php?id=${id}&view=vistas/personal/claro/estadoMsg.php&id_objeto=10031#no-back-button`;
+    console.log(this.data.url)
+  },
 });
